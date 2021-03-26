d1245d70cad3121d1e1fe6f6b426551dae177894
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-26 17:04:59 +0000

    Fixed errors

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import math
+
 playerChoice = ""
 diameter = 0
 area = 0
 circumference = 0
 
-print("Welcome to my calculator, here you can can calculate the circumference or area of a circle.")
-playerChoice = input("Would you like to calculate the area or the circumference of the circle: ")
+print(
+  "Welcome to my calculator, here you can can calculate the circumference or area of a circle."
+  )
+playerChoice = input(
+  "Would you like to calculate the area or the circumference of the circle: "
+  )
 
 if playerChoice.lower() == "area":
     playerChoice = int(input("Please input your diameter: "))
     diameter = playerChoice
-    area = math.pi * ((diameter / 2)**2)
+    area = math.pi * ((diameter / 2) ** 2)
     print(area)
 elif playerChoice.lower() == "circumference":
     playerChoice = int(input("Please input your diameter: "))
     diameter = playerChoice
-    circumference = 2 * math.pi * (diameter/2)
+    circumference = 2 * math.pi * (diameter / 2)
     print(circumference)
 else:
     print("Error this is not a option...")
